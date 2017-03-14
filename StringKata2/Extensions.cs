54005135308eb4bc7fba56313a3ef53c0deaf9ef
Antonio <zuntyrin@hotmail.com>
2017-03-14 20:42:18 +0200

    refactor : removed unused usings and cleaned up formatting.

--- StringKata2/Extensions.cs
+++ StringKata2/Extensions.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace StringKata2 {
     public static class Extensions {
@@ -11,7 +9,7 @@ namespace StringKata2 {
             return int.TryParse(value, out parsed) ? parsed : 0;
         }
         public static IEnumerable<int> ThrowExceptionForNegatives(this IEnumerable<int> numbers) {
-            if(numbers.Any(number=> number <0)) {
+            if (numbers.Any(number => number < 0)) {
                 throw new ArgumentOutOfRangeException(
                     $"No negatives allowed : {string.Join(",", numbers.Where(number => number < 0))}");
             } else {
