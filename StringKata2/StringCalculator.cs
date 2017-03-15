3b5551d24b3cb89376df24de55e0d0537be3a3b2
Antonio <zuntyrin@hotmail.com>
2017-03-15 05:36:51 +0200

    refactor : getDelimiters is now an expression to ease readability

--- StringKata2/StringCalculator.cs
+++ StringKata2/StringCalculator.cs
@@ -9,18 +9,14 @@ namespace StringKata2 {
         private const string delimiterPattern = @"(?<=\[)[^\]]*(?=\])|(?<=//).(?=\n)";
 
         internal int add(string input) {
-            return input.Split(getDelimiters(input), StringSplitOptions.None)
+            return input.Split(getDelimiters(input).ToArray(), StringSplitOptions.None)
                 .Select(number => number.asInt())
                 .ThrowExceptionForNegatives()
                 .Where(number => number <= 1000)
                 .Sum();
         }
 
-        private string[] getDelimiters(string input) {
-            return deriveCustomDelimiters(input)
-                .Union(delimiters)
-                .ToArray();
-        }
+        private IEnumerable<string> getDelimiters(string input) => deriveCustomDelimiters(input).Union(delimiters);
 
         private IEnumerable<string> deriveCustomDelimiters(string input) {
             return Regex.Matches(input, delimiterPattern).OfType<Match>()
